ccb2ea4763e74cd585323219e33cd1bb6f4995c7
Paul Foster <restofpaul@gmail.com>
2022-02-22 06:38:46 -0500

    Moved BallAuto, added comments

--- src/main/java/frc/robot/commands/auto/BallAuto.java
+++ src/main/java/frc/robot/commands/auto/BallAuto.java
@@ -1,8 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-package frc.robot.commands.drive;
+package frc.robot.commands.auto;
 
 import frc.robot.commands.cargo.Intake;
 import frc.robot.commands.cargo.LowerCargoElbow;
@@ -16,38 +12,41 @@ import frc.robot.subsystems.CargoSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.CargoSubsystem;
 
-/** Drives the robot autonomously in a plus sign pattern. */
+/** Autonomously collect one ball and score 2 balls in the lower hub. */
 public class BallAuto extends SequentialCommandGroup {
 
   /**
    * Create a new command.
-   *
-   * @param driveSubsystem The subsystem used by this command.
    */
   public BallAuto(DriveSubsystem driveSubsystem, CargoSubsystem cargoSubsystem) {
 
-    // Don't need to add the DriveSubsystem as a required subsystem because the
-    // commands
-    // scheduled will do that.
-
     addCommands(
-        // already have 1 ball preloaded
+        // Starting conditions:
+        //   1) Pointed toward the hub, bumper touching
+        //   2) 1 ball preloaded
+
+        // Drive backward and turn towards the ball to our back right
         new DriveStraight(driveSubsystem, -50),
         new SpinRightAngle(driveSubsystem, 178),
+
+        // Grab the ball
         new ParallelCommandGroup(
             new LowerCargoWrist(cargoSubsystem),
             new DriveStraight(driveSubsystem, 30)),
-        new LowerCargoWrist(cargoSubsystem),
-        new DriveStraight(driveSubsystem, 30),
         new Intake(cargoSubsystem).withTimeout(5),
+ 
+        // Return to the hub
         new RaiseCargoWrist(cargoSubsystem),
         new SpinRightAngle(driveSubsystem, -178),
         new LowerCargoWrist(cargoSubsystem),
         new RaiseCargoElbow(cargoSubsystem),
         new DriveStraight(driveSubsystem, 70),
+
+        // Deposit 2 balls in the hub
         new Outtake(cargoSubsystem).withTimeout(5),
+
+        // NATE: What were we doing here?
         new DriveStraight(driveSubsystem, -50),
         new SpinRightAngle(driveSubsystem, 90),
         new DriveStraight(driveSubsystem, 50),
