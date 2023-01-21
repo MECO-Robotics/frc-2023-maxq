78e2133a79ef89508dc06303bc13e7e88ed7a1cd
Paul Foster <restofpaul@gmail.com>
2023-01-21 13:33:47 +0000

    Arm subsystem

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,14 +1,49 @@
 package frc.robot.subsystems;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTableListener;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
+
+    NetworkTable fixedVisionCameraTable;
+
+    DoubleSubscriber ySub;
+    // use an AtomicReference to make updating the value thread-safe
+    AtomicReference<Double> yValue = new AtomicReference<Double>();
+    // retain listener handles for later removal
+    int connListenerHandle;
+    int valueListenerHandle;
+    int topicListenerHandle;
+
     
-    public class RobotPoseSample{
+    public class RobotPoseSample {
+
         public Pose2d robotPose;
         public double timeStamp;
+    }
+
+    public VisionSubsystem() {
+
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+
+        fixedVisionCameraTable = inst.getTable("field-camera");
+
+        // add a connection listener; the first parameter will cause the
+        // callback to be called immediately for any current connections
+        // connListenerHandle = inst.addConnectionListener(true, event -> {
+        //     if (event.is(NetworkTableEvent.Kind.kConnected)) {
+        //         System.out.println("Connected to " + event.connInfo.remote_id);
+        //     } else if (event.is(NetworkTableEvent.Kind.kDisconnected)) {
+        //         System.out.println("Disconnected from " + event.connInfo.remote_id);
+        //     }
+        // });
+
     }
 
     @Override
@@ -17,7 +52,7 @@ public class VisionSubsystem extends SubsystemBase {
     }
 
     RobotPoseSample getVisionMeasurement() {
-        
+
         return null;
     }
 }
