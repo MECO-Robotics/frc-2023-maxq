0159ecc6257c62bbbf09bbf739b8a8f8e5c6307a
Paul Foster <restofpaul@gmail.com>
2023-03-11 18:49:40 +0000

    copyright notice

--- src/main/java/frc/robot/commands/arm/ArmStow.java
+++ src/main/java/frc/robot/commands/arm/ArmStow.java
@@ -1,9 +1,13 @@
+// Copyright (c) MECO Robotics
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 package frc.robot.commands.arm;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.Constants;
 import frc.robot.Constants.ElbowPosition;
+import frc.robot.Constants.ShoulderPosition;
 
 public class ArmStow extends CommandBase {
 
@@ -17,8 +21,8 @@ public class ArmStow extends CommandBase {
 
     @Override
     public void execute() {
-        armSubsystem.move(Constants.ElbowPosition.allIn);
-        armSubsystem.move(Constants.ShoulderPosition.allBackStow);
+        armSubsystem.move(ElbowPosition.allIn);
+        armSubsystem.move(ShoulderPosition.allBackStow);
     }
 
     @Override
